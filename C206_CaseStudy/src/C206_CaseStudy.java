c48afc4d3de7b89255c134f85d8775e2b3d919c1
19044525 <19044525@DESKTOP-KS53TMD>
2020-08-19 20:35:38 +0800

    update addMenuItem() in C206_CaseStudy update deleteMenuItem() in 206_CaseStudy update viewAllMenuItem() in 206_CaseStudy update MenuItem class update Helper class

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,95 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<MenuItem> MenuItemList = new ArrayList<MenuItem>();
+		
+		int option = 0;
+		
+		while (option !=4) {
+			C206_CaseStudy.optionMenu();
+			option = Helper.readInt("Enter Option > ");
+			
+			if(option == 1) {
+				// add menu item
+				C206_CaseStudy.addMenuItem(MenuItemList);
+				
+			}else if(option == 2) {
+				//delete menu item
+				C206_CaseStudy.deleteMenuItem(MenuItemList);
+				
+			} else if (option == 3) {
+				// view menu item
+				C206_CaseStudy.viewAllMenuItem(MenuItemList);
+			
+			} else {
+				System.out.println("Invalid Option");
+			}
+		}
+		System.out.println("Thank You");
+	}
+	
+	public static void optionMenu() {
+		Helper.line(60,"=");
+		System.out.println("School Lunch App");
+		Helper.line(60,"=");
+		System.out.println("1. Add Menu Item");
+		System.out.println("2. Delete Menu Item");
+		System.out.println("3. View Menu Item");
+		System.out.println("4. Quit");
+	}
+	
+	// ==================================== Add menu item ==============================================
+	public static void addMenuItem(ArrayList<MenuItem> MenuItemList) {
+		Helper.line(60,"=");
+		System.out.println("ADD MENU ITEM");
+		Helper.line(60,"=");
+		
+		String category = Helper.readString("Enter Item's Category: ");
+		String name = Helper.readString("Enter Item's Name: ");
+		boolean healthyChoice = Helper.readBoolean("Is The Item A Healthier Choice? (Y/N) ");
+		double price = Helper.readDouble("Enter Item's Price: ");
+		
+		MenuItem menuItem = new MenuItem(category, name, healthyChoice, price);
+		MenuItemList.add(menuItem);
+		System.out.println("Menu Item Added");
 	}
 
+	//===================================== delete menu item ===========================================
+	public static void deleteMenuItem(ArrayList<MenuItem> MenuItemList) {
+		Helper.line(60,"=");
+		System.out.println("DELETE MENU ITEM");
+		Helper.line(60,"=");
+		
+		String itemDelete = Helper.readString("Enter Item's Name To Delete: ");
+		
+		for (int i = 0; i < MenuItemList.size(); i++) {
+			if(MenuItemList.get(i).getName().toLowerCase().equalsIgnoreCase(itemDelete)) {
+				MenuItemList.remove(i);
+				System.out.println("Item Deleted");
+			} else {
+				System.out.println("Item Not Deleted");
+			}
+		}
+	}
+	
+	//===================================== view menu item =============================================
+	public static void viewAllMenuItem(ArrayList<MenuItem> MenuItemList) {
+		Helper.line(60,"=");
+		System.out.println("VIEW MENU ITEM");
+		Helper.line(60,"=");
+		
+		String output = "";
+		
+		output += String.format("%-10s %-10s %-20s %-10s\n", "CATEGORY", "NAME", "HEALTHY CHOICE", "PRICE");
+		for (int i = 0; i < MenuItemList.size(); i++) {
+			output += String.format("%-10s %-10s %-20b %-10s\n", MenuItemList.get(i).getCategory(), MenuItemList.get(i).getName(), 
+					MenuItemList.get(i).isHealthyChoice(),MenuItemList.get(i).getPrice());
+		}
+		System.out.println(output);
+	}
+	
 }
+
+
---
+++ C206_CaseStudy/src/MenuItem.java
@@ -0,0 +1,52 @@
+public class MenuItem {
+	private String category;
+	private String name;
+	private boolean healthyChoice;
+	private double price;
+	
+	public MenuItem (String category, String name, boolean healthyChoice, double price) {
+		this.category=category;
+		this.name=name;
+		this.healthyChoice=healthyChoice;
+		this.price=price;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public  void setCategory() {
+		this.category=category;
+	}
+	
+	public boolean isHealthyChoice() {
+		return healthyChoice;
+	}
+	public void setHealthyChoice(boolean healthyChoice) {
+		healthyChoice = false;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName() {
+		this.name=name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice() {
+		this.price=price;
+	}
+	
+	public String toString() {
+		String output ="";
+		output += "Category: " + category + "" + "Name: " + name + "" + "Healthy Choice: " + healthyChoice + "" + "Price: " + price;
+		return output;
+	}
+	
+
+}
