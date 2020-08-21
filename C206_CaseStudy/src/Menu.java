d9d9394bca159931aac17d057b07baefcc4f3bb7
19034926 <19034926@DESKTOP-50K3SGF>
2020-08-21 22:03:03 +0800

    Added Menu object

---
+++ C206_CaseStudy/src/Menu.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+public class Menu {
+	private String displayName;
+	private int month;
+	private int numberOfItems;
+	private ArrayList<MenuItem> items;
+	public Menu(String displayName, int month, int numberOfItems, ArrayList<MenuItem> items) {
+		super();
+		this.displayName = displayName;
+		this.month = month;
+		this.numberOfItems = numberOfItems;
+		this.items = items;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getNumberOfItems() {
+		return numberOfItems;
+	}
+	public void setNumberOfItems(int numberOfItems) {
+		this.numberOfItems = numberOfItems;
+	}
+	public ArrayList<MenuItem> getItems() {
+		return items;
+	}
+	public void setItems(ArrayList<MenuItem> items) {
+		this.items = items;
+	}
+	public String toString() {
+		String output="";
+		String menuItems="";
+		for(MenuItem i:items) {
+			menuItems+=i.getName()+", ";
+		}
+		output+=String.format("%-20s %-10d %-10d %s\n", displayName,month,numberOfItems,menuItems);
+		return output;
+	}
+	
+}
